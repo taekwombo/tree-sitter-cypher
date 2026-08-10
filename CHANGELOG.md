b49c27d8a10576eb40dc64f950cfb6f99220be24
taekwombo <88032359+taekwombo@users.noreply.github.com>
2026-08-10 06:59:36 +0200

    feat(2024.3): update numeric literal grammar

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+### 2024.3 GQL
+
+#### Numbers:
+- INF, INFINITY, NAN are now valid numeric literals
+- added support for `_` separator
+- 007 is now valid decimal integer
+
+#### Grammar Nodes:
+Renamed nodes:
+- `double_literal` -> `decimal_literal`
+
+Removed nodes:
+- `exponent_decimal_real`
+- `regular_decimal_real`
--- grammar.ts
+++ grammar.ts
@@ -1,6 +1,27 @@
 // opencypher tests
 // https://github.com/opencypher/openCypher/blob/c816756d50df9cde73cae573ef871f2d7e76c70a/tools/grammar/src/test/resources/cypher.txt
 
+const OCT_INT = /0o(_?[0-7])+/;
+const HEX_INT = /0x(_?[0-9a-f])+/i;
+const DEC_INT = /[0-9](_?[0-9])*/;
+const DECIMAL =  token.immediate(seq(
+    optional(DEC_INT),
+    '.',
+    DEC_INT,
+));
+const DECIMAL_SCIENTIFIC = token.immediate(seq(
+    choice(DEC_INT, DECIMAL),
+    choice('e', 'E'),
+    optional(choice('-', '+')),
+    DEC_INT,
+));
+const DECIMAL_LITERAL = token.immediate(seq(
+    choice(DECIMAL, DECIMAL_SCIENTIFIC),
+    optional(
+        choice('f', 'F', 'd', 'D'),
+    )
+));
+
 module.exports = grammar({
     name: 'cypher',
     extras: ($) => [$.comment, $._whitespace_char],
@@ -637,8 +658,11 @@ module.exports = grammar({
             /[U][a-fA-F0-9]{8}/,
         ))),
         number_literal: ($) => choice(
-            $.double_literal,
+            $.decimal_literal,
             $.integer_literal,
+            word('inf'),
+            word('infinity'),
+            word('nan'),
         ),
         map_literal: ($) => seq(
             '{',
@@ -666,34 +690,10 @@ module.exports = grammar({
             $.octal_integer,
             $.decimal_integer,
         ),
-        hex_integer: () => /0x[0-9a-f]+/i,
-        decimal_integer: () => choice(
-            '0',
-            /[1-9][0-9]*/,
-        ),
-        octal_integer: () => /0o[0-7]+/,
-        double_literal: ($) => choice(
-            $.exponent_decimal_real,
-            $.regular_decimal_real,
-        ),
-        exponent_decimal_real: () => token(seq(
-            choice(
-                /[0-9]+/,
-                seq(
-                    /[0-9]+/,
-                    '.',
-                    /[0-9]+/,
-                ),
-                seq(
-                    '.',
-                    /[0-9]+/,
-                ),
-            ),
-            word('e'),
-            optional('-'),
-            /[0-9]+/,
-        )),
-        regular_decimal_real: () => /[0-9]*\.[0-9]+/,
+        hex_integer:     ($) => HEX_INT,
+        decimal_integer: ($) => DEC_INT,
+        octal_integer:   ($) => OCT_INT,
+        decimal_literal: ($) => DECIMAL_LITERAL,
         schema_name: ($) => choice($.symbolic_name, $.reserved_word),
         reserved_word: () => choice(
             word('all'),
